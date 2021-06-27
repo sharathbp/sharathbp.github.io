c5858263255fbffed2c2541da75168c8d4133b52
sharath <sharathbp@onedirect.in>
2021-06-28 01:42:23 +0530

    gatsby portfolio'

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,6 +4,7 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/sharathbp/sharathbp.github.io/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/Users/sharathbp/sharathbp.github.io/src/pages/404.js")),
   "component---src-pages-home-js": preferDefault(require("/Users/sharathbp/sharathbp.github.io/src/pages/home.js")),
   "component---src-pages-index-js": preferDefault(require("/Users/sharathbp/sharathbp.github.io/src/pages/index.js"))
